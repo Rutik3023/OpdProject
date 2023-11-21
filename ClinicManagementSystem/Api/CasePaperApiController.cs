cfd5ab618720d4b89696e118583d9f586ac8ba5f
Kedar Kurade <kedar3736@gmail.com>
2023-11-21 20:22:16 +0530

    Kedar CasePaper Api Controller

---
+++ ClinicManagementSystem/Api/CasePaperApiController.cs
@@ -0,0 +1,129 @@
+﻿using AutoMapper;
+using ClinicManagementSystem.Iservises;
+using ClinicManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicManagementSystem.Api
+{
+    public class CasePaperApiController : Controller
+    {
+        // GET: CasePaperApi
+        ICasePaper c;
+        Mapper mp = null;
+        public CasePaperApiController(ICasePaper _c)
+        {
+            var config = new MapperConfiguration(u => u.CreateMap<VMCasePaper, tblAppointment>().ReverseMap());
+            mp = new Mapper(config);
+
+            c = _c;
+
+        }
+
+
+        // GET: Api
+
+
+        public JsonResult GetAllC(int pagno)
+        {
+            Reports rp = new Reports();
+
+            var obj = c.GetAll();
+            var objt = c.GetAllPage(pagno);
+
+            List<VMCasePaper> list = mp.Map<List<VMCasePaper>>(objt);
+
+
+
+
+            rp.pageno = pagno;
+            rp.count = obj.Count();
+            rp.Code = 0;
+            rp.Message = list;
+
+            return Json(rp, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult FindByid(int id)
+        {
+            Reports rp = new Reports();
+
+            var obj = c.FindById(id);
+            VMCasePaper list = mp.Map<VMCasePaper>(obj);
+
+            rp.Code = 0;
+            rp.Message = list;
+            return Json(rp, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Save(VMAppointment obj)
+        {
+            Reports rp = new Reports();
+
+            tblCasePaper list = mp.Map<tblCasePaper>(obj);
+            var res = c.Save(list);
+
+
+
+            rp.Code = 0;
+            rp.Message = res;
+            //if (ModelState.IsValid)
+            //{
+
+
+
+
+
+
+
+            //}
+            //else
+            //{
+            //    rp.Code = -1;
+            //    rp.Message = CommonRepo.GetAdditionValidaitonIssues(ModelState);
+            //}
+
+
+
+
+
+
+            return Json(rp, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Delete(int id)
+        {
+            Reports rp = new Reports();
+            var obj = c.Delete(id);
+            if (obj == true)
+            {
+                rp.Code = 0;
+                rp.Message = "Deleted  Successfully";
+            }
+
+
+
+            return Json(rp, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetAll(string key)
+        {
+            Reports rp = new Reports();
+
+            var obj = c.GetAllKey(key);
+
+            if (obj != null)
+            {
+                rp.Code = 0;
+                rp.Message = obj;
+            }
+
+            return Json(rp, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
