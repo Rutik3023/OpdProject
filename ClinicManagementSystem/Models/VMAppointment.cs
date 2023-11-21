e020db018b595e6b30735f46a0047b8db1d599b2
surajTE <eyalamsuraj7227@gmail.com>
2023-11-21 21:26:10 +0530

    Appointment View and modal changes

--- ClinicManagementSystem/Models/VMAppointment.cs
+++ ClinicManagementSystem/Models/VMAppointment.cs
@@ -13,5 +13,10 @@ namespace ClinicManagementSystem.Models
         public int Status { get; set; }
         public DateTime AppointmentDate { get; set; }
         public DateTime CreatedOn { get; set; }
+        public string HealthIssue { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public string BP { get; set; }
+        public string PId { get; set; }
     }
 }
