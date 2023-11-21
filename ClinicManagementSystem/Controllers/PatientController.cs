2f3069643edd85c115f11aa57a2d09e032b7e817
Rutik3023 <rutikthorat3023@gmail.com>
2023-11-21 21:40:26 +0530

    rutikchange

--- ClinicManagementSystem/Controllers/PatientController.cs
+++ ClinicManagementSystem/Controllers/PatientController.cs
@@ -8,12 +8,13 @@ using System.Web.Mvc;
 namespace ClinicManagementSystem.Controllers
 {
     public class PatientController : Controller
-    {
+    { HospitalDBEntities db = new HospitalDBEntities();
        
         // GET: Patient
         public ActionResult Index()
         {
             ViewBag.blood = CommonRepo.Blood();
+            TempData["count"] = db.tblPatients.Count();
             return View();
         }
     }
