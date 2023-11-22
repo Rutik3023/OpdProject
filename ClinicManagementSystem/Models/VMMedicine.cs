1a7b373cf39cf32dd4c825ce1ec1da478a216172
surajTE <eyalamsuraj7227@gmail.com>
2023-11-22 16:42:17 +0530

    Medicine View Added

---
+++ ClinicManagementSystem/Models/VMMedicine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicManagementSystem.Models
+{
+    public class VMMedicine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MContent { get; set; }
+        public string Company { get; set; }
+    }
+}
