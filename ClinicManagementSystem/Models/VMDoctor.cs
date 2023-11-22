417bf2c3cfa19f6655ffd9ede25ea2260d946f22
Nilesh <Nilesh@LAPTOP-9JI4EJFU>
2023-11-22 16:25:42 +0530

    nilesh

--- ClinicManagementSystem/Models/VMDoctor.cs
+++ ClinicManagementSystem/Models/VMDoctor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,16 +9,38 @@ namespace ClinicManagementSystem.Models
     public class VMDoctor
     {
         public int Id { get; set; }
+
+        [Required]
         public int ClinicId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string Mobile { get; set; }
+
+        [Required]
         public string Address { get; set; }
+
+        [Required]
         public string Qulification { get; set; }
+
+        [Required]
         public string Specilization { get; set; }
+
+        [Required]
         public char Gender { get; set; }
+
+        [Required]
         public int Role { get; set; }
+
+        [Required]
         public string UserName { get; set; }
+
+        [Required]
         public string Password { get; set; }
         public string Photo { get; set; }
         public int CreatedBy { get; set; }
