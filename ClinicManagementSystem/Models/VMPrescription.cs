c835f5a39044d51234c0869c974a2e9700f792a1
surajTE <eyalamsuraj7227@gmail.com>
2023-11-23 22:59:30 +0530

    AdminLayout some Change, Prescription Modal,View and controller add --Suraj

---
+++ ClinicManagementSystem/Models/VMPrescription.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicManagementSystem.Models
+{
+    public class VMPrescription
+    {
+        public int Id { get; set; }
+        public int AId { get; set; }
+        public string Narration { get; set; }
+        public DateTime Createdon { get; set; }
+        public string Symtams { get; set; }
+        public string Disiese { get; set; }
+        public int MedicineId { get; set; }
+        public int QTY { get; set; }
+        public bool MDose { get; set; }
+        public bool AfDose { get; set; }
+        public bool NDose { get; set; }
+        public string Stastus { get; set; }
+    }
+}
