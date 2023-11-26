7144bf51115cc7002318a518c46dc58e46c14a1f
surajTE <eyalamsuraj7227@gmail.com>
2023-11-26 21:11:18 +0530

    Print Recipt View Index controller

---
+++ ClinicManagementSystem/Controllers/PrintController.cs
@@ -0,0 +1,19 @@
+﻿using Rotativa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicManagementSystem.Controllers
+{
+    public class PrintController : Controller
+    {
+        // GET: Print
+        public ActionResult PrintData()
+        {
+            var pdf = new ViewAsPdf("PrintData"); // StaticPage is the name of your HTML view
+            return pdf;
+        }
+    }
+}
